9bb09f4cd74fe18f387b241570eba12e13a981bb
Gabriel Vincent <gabriel@vincent.es>
2024-02-22 14:57:38 +0000

    add colorschemes

--- nvim/lua/gabrielvincent/colors.lua
+++ nvim/lua/gabrielvincent/colors.lua
@@ -1,13 +1,14 @@
-require('tokyonight').setup({day_brightness = 0.5})
+-- require('tokyonight').setup({day_brightness = 0.35})
 local auto_dark_mode = require('auto-dark-mode')
 
 auto_dark_mode.setup({
-    update_interval = 60 * 30 * 1000,
-    set_dark_mode = function() vim.api.nvim_set_option('background', 'dark') end,
+    update_interval = 1000,
+    set_dark_mode = function() vim.cmd("colorscheme tokyonight-moon") end,
     set_light_mode = function()
-        vim.api.nvim_set_option('background', 'light')
+        -- vim.api.nvim_set_option('background', 'light')
+        vim.cmd("colorscheme catppuccin-latte")
     end
 })
 
-vim.cmd("colorscheme tokyonight-moon")
+-- vim.cmd("colorscheme tokyonight-moon")
 
--- nvim/lua/gabrielvincent/packer.lua
+++ nvim/lua/gabrielvincent/packer.lua
@@ -1,18 +1,18 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling `lua require('plugins')` from your init.vimpackep-z
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    use "wbthomason/packer.nvim"
     use {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.3',
-        -- or                            , branch = '0.1.x',
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.3",
+        -- or                            , branch = "0.1.x",
         requires = {
-            {'nvim-lua/plenary.nvim'},
+            {"nvim-lua/plenary.nvim"},
             {"nvim-telescope/telescope-live-grep-args.nvim"}
         },
         config = function()
@@ -20,17 +20,18 @@ return require('packer').startup(function(use)
         end
     }
     use {"folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}}
+    use {"catppuccin/nvim", as = "catppuccin"}
     use('nvim-treesitter/nvim-treesitter')
-    use('mbbill/undotree')
-    use('theprimeagen/vim-be-good')
-    use('tpope/vim-fugitive')
+    use("mbbill/undotree")
+    use("theprimeagen/vim-be-good")
+    use("tpope/vim-fugitive")
     use {
         -- Configure tailwindcss language server
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
         requires = {
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'}, {'hrsh7th/cmp-nvim-lsp'}
+            {"hrsh7th/nvim-cmp"}, {"hrsh7th/cmp-nvim-lsp"}
         }
     }
     use({
@@ -41,37 +42,38 @@ return require('packer').startup(function(use)
         run = "make install_jsregexp"
     })
     use {"williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim"}
-    use('nvimdev/lspsaga.nvim')
-    use('neovim/nvim-lspconfig')
-    use('MunifTanjim/prettier.nvim')
-    use('sbdchd/neoformat')
-    use('m4xshen/autoclose.nvim')
-    use('romainl/vim-cool')
+    use("nvimdev/lspsaga.nvim")
+    use("neovim/nvim-lspconfig")
+    use("MunifTanjim/prettier.nvim")
+    use("sbdchd/neoformat")
+    use("m4xshen/autoclose.nvim")
+    use("romainl/vim-cool")
     -- use("github/copilot.vim")
     use("Exafunction/codeium.vim")
     use("theprimeagen/harpoon")
     use("tjdevries/stackmap.nvim")
     use("lukas-reineke/indent-blankline.nvim")
-    use('HiPhish/rainbow-delimiters.nvim')
-    use('nvim-treesitter/nvim-treesitter-context')
-    use('windwp/nvim-ts-autotag')
-    use('lewis6991/gitsigns.nvim')
+    use("HiPhish/rainbow-delimiters.nvim")
+    use("nvim-treesitter/nvim-treesitter-context")
+    use("windwp/nvim-ts-autotag")
+    use("lewis6991/gitsigns.nvim")
     use {
-        'nvim-lualine/lualine.nvim',
+        "nvim-lualine/lualine.nvim",
         requires = {
-            'nvim-tree/nvim-web-devicons',
-            'ryanoasis/vim-devicons',
+            "nvim-tree/nvim-web-devicons",
+            "ryanoasis/vim-devicons",
             opt = true
         }
     }
     use("numtoStr/Comment.nvim")
     use("JoosepAlviste/nvim-ts-context-commentstring")
-    use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
+    use {"kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async"}
     use("tpope/vim-surround")
     -- use("nathom/filetype.nvim")
     use("RRethy/vim-illuminate")
     use("f-person/auto-dark-mode.nvim")
     use("stevearc/oil.nvim")
+    use {"folke/trouble.nvim", requires = "nvim-tree/nvim-web-devicons"}
 
     -- Local plugins
     -- use("~/Code/nvim/plugins/slate.nvim")
