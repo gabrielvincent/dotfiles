c0c586e2e11c2c198ec6570554392fbd892971a8
Gabriel Vincent <gabriel@vincent.es>
2024-05-17 16:15:19 +0100

    prepare for neovim 0.10: fine-tune keybindings

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -5,10 +5,10 @@ lspsaga.setup({
 	diagnostic = { keys = { quit = { "q", "<ESC>" } } },
 })
 
-vim.keymap.set("n", "<leader>e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
 vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
 vim.keymap.set("n", "<leader>df", "<cmd>Lspsaga peek_definition<CR>")
-vim.keymap.set("n", "<leader>k", "<cmd>Lspsaga hover_doc<CR>")
-vim.keymap.set("n", "<leader>tr", "<cmd>Lspsaga term_toggle<CR>")
-vim.keymap.set("t", "<leader>tr", "<cmd>Lspsaga term_toggle<CR>")
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
 vim.keymap.set("n", "<leader>i", "<cmd>Lspsaga incoming_calls<CR>")
+vim.keymap.set("n", "<C-W>d", "<cmd>Lspsaga show_line_diagnostics<CR>")
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<C-f>", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gd", builtin.lsp_document_symbols, {})
 vim.keymap.set("n", "<C-s>", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
