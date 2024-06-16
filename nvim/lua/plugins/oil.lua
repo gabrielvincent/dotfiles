8910af7f2899137e620de3cabafbf9b0a2b24b4d
Gabriel Vincent <gabriel@vincent.es>
2024-06-16 12:25:07 +0100

    change colroscheme

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,14 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 return {
   "stevearc/oil.nvim",
-  opts = {},
+  opts = {
+    ["<C-s>"] = nil,
+    ["<C-h>"] = nil,
+    ["<C-t>"] = nil,
+    ["<C-p>"] = nil,
+    ["<C-c>"] = nil,
+    ["<C-l>"] = nil,
+  },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
