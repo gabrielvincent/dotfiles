3e54553982dfb7e5d911cd97409ed88156573bb0
Gabriel Vincent <gabriel@vincent.es>
2025-06-05 17:25:39 -0300

    neovim: set biome as formatter for TypeScript

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
   opts = function(_, opts)
     opts.formatters_by_ft = opts.formatters_by_ft or {}
     opts.formatters_by_ft.javascript = { "prettierd" }
-    opts.formatters_by_ft.typescript = { "prettierd" }
+    opts.formatters_by_ft.typescript = { "biome", "biome-organize-imports" }
     opts.formatters_by_ft.svelte = { "prettierd" }
     opts.formatters_by_ft.liquid = { "prettierd" }
     opts.formatters_by_ft.templ = { "templ" }
