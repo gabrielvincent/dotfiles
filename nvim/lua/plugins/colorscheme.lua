acb76d19921a726eb892f74867c21f1499edcbe6
Gabriel Vincent <gabriel@vincent.es>
2025-08-20 16:44:26 -0300

    neovim: update colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,13 @@
 return {
-  { "catppuccin/nvim" },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "light",
+      })
+      -- Enable theme
+      require("onedark").load()
+    end,
   },
 }
