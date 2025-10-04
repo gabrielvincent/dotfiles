59f4aff7ea5c9aa8da7b8865263feb20b9fdcaab
Gabriel Vincent <gabriel@vincent.es>
2025-10-04 07:52:23 -0300

    neovim: replace oil with fyler

--- nvim/lua/plugins/_disabled.lua
+++ nvim/lua/plugins/_disabled.lua
@@ -2,4 +2,5 @@ return {
   -- disable bufferline
   { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,15 @@
 return {
   "snacks.nvim",
   opts = function(_, opts)
+    opts.dashboard = { enabled = true }
     opts.scroll = { enabled = false }
+    opts.explorer = { enabled = false }
+    opts.picker = { enabled = false }
   end,
+  keys = {
+    { "<leader>fe", false },
+    { "<leader>fE", false },
+    { "<leader>E", false },
+    { "<leader>e", false },
+  },
 }
