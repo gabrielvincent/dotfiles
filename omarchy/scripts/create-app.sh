1059fb277c4770082771209eb661902b757b3983
Gabriel Vincent <gabriel@vincent.es>
2025-10-10 15:03:19 -0300

    create calendar app

--- omarchy/scripts/create-app.sh
+++ omarchy/scripts/create-app.sh
@@ -6,16 +6,16 @@ if [ -z "$1" ] || [ -z "$2" ]; then
     echo ""
     echo "Missing:"
     [ -z "$1" ] && echo "  - app_name (parameter 1)"
-    [ -z "$2" ] && echo "  - app_url (parameter 2)"
+    [ -z "$2" ] && echo "  - class (parameter 2)"
     echo ""
-    echo "Usage: $0 <app_name> <app_url>"
-    echo "Example: $0 myapp example.com"
+    echo "Usage: $0 <app_name> <class>"
+    echo "Example: $0 myapp myclass"
     exit 1
 fi
 
 app_name=$1
-app_url="chrome-""$2""__-Default"
+app_class=$2
 
 mv ~/.local/share/applications/"$app_name".desktop ~/.config/omarchy/applications
-echo "StartupWMClass=$app_url" >> ~/.config/omarchy/applications/"$app_name".desktop
+echo "StartupWMClass=$app_class" >> ~/.config/omarchy/applications/"$app_name".desktop
 ln -s ~/.config/omarchy/applications/"$app_name".desktop ~/.local/share/applications/"$app_name".desktop
