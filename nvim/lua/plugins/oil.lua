fab6770aed98b670afc09c4855472a4e46362d16
Gabriel Vincent <gabriel@vincent.es>
2025-10-11 08:45:18 -0300

    neovim: replace fyler with oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -8,20 +8,20 @@ return {
   cmd = "Oil",
   lazy = false,
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- keys = {
-  --   {
-  --     "-",
-  --     function()
-  --       if should_open_oil() == false then
-  --         return
-  --       end
-  --       vim.cmd.Oil()
-  --     end,
-  --     desc = "Open parent directory",
-  --   },
-  -- },
+  keys = {
+    {
+      "-",
+      function()
+        if should_open_oil() == false then
+          return
+        end
+        vim.cmd.Oil()
+      end,
+      desc = "Open parent directory",
+    },
+  },
   opts = {
-    default_file_explorer = false,
+    default_file_explorer = true,
     view_options = {
       show_hidden = true,
     },
