471283dc5b039be48807a0eecfe76c25b699c0a7
Gabriel Vincent <gabriel@vincent.es>
2025-10-11 08:36:19 -0300

    neovim: sync theme with omarchy

--- nvim/lua/plugins/_disabled.lua
+++ nvim/lua/plugins/_disabled.lua
@@ -3,4 +3,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
 }
---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/vin/.config/omarchy/current/theme/neovim.lua
