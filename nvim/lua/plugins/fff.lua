471021f78ee59140d51a35174f973b1dc3757b94
Gabriel Vincent <gabriel@vincent.es>
2026-05-14 10:35:57 -0300

    neovim: update plugins

--- nvim/lua/plugins/fff.lua
+++ nvim/lua/plugins/fff.lua
@@ -1,35 +1,47 @@
 return {
-  "dmtrKovalenko/fff.nvim",
+  "dmtrKovalenko/fff",
+  enabled = true,
   build = function()
-    -- this will download prebuild binary or try to use existing rustup toolchain to build from source
-    -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+    -- downloads a prebuilt binary or falls back to cargo build
     require("fff.download").download_or_build_binary()
   end,
-  -- if you are using nixos
+  -- for nixos:
   -- build = "nix run .#release",
-  opts = { -- (optional)
+  opts = {
     debug = {
-      enabled = true, -- we expect your collaboration at least during the beta
-      show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
-    },
-    keymaps = {
-      cycle_previous_query = "<Up>",
-      move_up = "<C-p>",
-    },
-    git = {
-      status_text_color = true,
+      enabled = false,
+      show_scores = false,
     },
   },
-  -- No need to lazy-load with lazy.nvim.
-  -- This plugin initializes itself lazily.
-  lazy = false,
+  lazy = true, -- the plugin lazy-initialises itself
   keys = {
     {
-      "ff", -- try it if you didn't it is a banger keybinding for a picker
+      "ff",
       function()
         require("fff").find_files()
       end,
       desc = "FFFind files",
+    },
+    {
+      "fg",
+      function()
+        require("fff").live_grep()
+      end,
+      desc = "LiFFFe grep",
+    },
+    {
+      "fz",
+      function()
+        require("fff").live_grep({ grep = { modes = { "fuzzy", "plain" } } })
+      end,
+      desc = "Live fffuzy grep",
+    },
+    {
+      "fc",
+      function()
+        require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+      end,
+      desc = "Search current word",
     },
   },
 }
