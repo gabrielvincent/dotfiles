09949af04c19f3b70ce6bfb1059116c950d37c90
Gabriel Vincent <gabriel@vincent.es>
2026-08-21 18:25:58 -0300

    neovim: disable wrapwidth for markdown

--- nvim/lua/plugins/wrapwidth.lua
+++ nvim/lua/plugins/wrapwidth.lua
@@ -3,18 +3,5 @@ return {
   init = function()
     vim.g.wrapwidth_sign = "↵"
     -- vim.g.wrapwidth_number = 1
-    vim.api.nvim_create_augroup("MarkdownWrap", { clear = true })
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufLeave" }, {
-      group = "MarkdownWrap",
-      pattern = "*.md",
-      callback = function(event)
-        if event.event == "BufEnter" then
-          vim.cmd("Wrapwidth 80")
-          vim.opt.wrap = true
-        else
-          vim.opt.wrap = false
-        end
-      end,
-    })
   end,
 }
